196ddaaebc96b5b5e7c857172aef703259840cb9
Kurosh <Kurosh>
2017-02-25 21:43:14 -0700

    Initial Implementation of Vue Display App

---
+++ js/next.js
@@ -0,0 +1,29 @@
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        users: [
+            'a', 'b', 'c','d'
+        ],
+        fields: [
+            {
+                cost: 2,
+                name: 'yeah',
+                quantity: 2,
+                userIndex: -1
+            },
+            {
+                cost: 3,
+                name: 'no',
+                quantity: 3,
+                userIndex: -1
+            }
+        ],
+        selectedTab: 0
+    },
+    methods:{
+
+    },
+
+
+});
