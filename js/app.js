cd9276d182792e67a5f67895ebeb7cfd095fade7
Matt Lyons <matt@mattlyons.net>
2017-02-25 18:44:53 -0700

    Implement Photo Taking on Mobile Devices

--- js/app.js
+++ js/app.js
@@ -2,69 +2,25 @@
 var app = new Vue({
     el: '#app',
     data: {
-        show : false
+        showImage: false
     },
     methods:{
-        getimage:function(){
-            "use strict";
-            function hasGetUserMedia() {
-                return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                navigator.mozGetUserMedia || navigator.msGetUserMedia);
-            }
-            if (hasGetUserMedia()) {
 
-                var video = document.querySelector('video');
-                var canvas = document.querySelector('canvas');
-                var ctx = canvas.getContext('2d');
-                var localMediaStream = null;
-
-                var hdConstraints = {
-                    video: {
-                        mandatory: {
-                            minWidth: 1280,
-                            minHeight: 720
-                        }
-                    }
-                };
-
-                video.addEventListener('click', function(){
-                    if (localMediaStream) {
-                    ctx.drawImage(video, 0, 0);
-                    // "image/webp" works in Chrome.
-                    // Other browsers will fall back to image/png.
-                    document.querySelector('img').src = canvas.toDataURL('image/webp');
-                }}, false)
-
-                var errorCallback = function(e) {
-                    console.log('Rejected!', e);
-                };
-
-                navigator.getUserMedia  = navigator.getUserMedia ||
-                    navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia ||
-                    navigator.msGetUserMedia;
-                if (navigator.getUserMedia) {
-                    navigator.getUserMedia(hdConstraints, function(stream) {
-                        video.src = window.URL.createObjectURL(stream);
-                        localMediaStream = stream;
-                    }, errorCallback);
-                } else {
-                    alert("Come back with a better device");
-                }
-            }
-            else {
-                alert('getUserMedia() is not supported in your browser');
-            }
-        }
     }
 
-})
+});
 
-function snapshot() {
-    if (localMediaStream) {
-        ctx.drawImage(video, 0, 0);
-        // "image/webp" works in Chrome.
-        // Other browsers will fall back to image/png.
-        document.querySelector('img').src = canvas.toDataURL('image/webp');
-    }
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+      document.querySelector('#image').src = e.target.result;
+      data.imageURL = e.target.result;
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+    app.showImage = true;
+  }
 }
