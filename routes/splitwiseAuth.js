e30e0ad0a57bbd9e1df10ca22861bce742d8ae1d
Nathan <nyellow1@asu.edu>
2017-02-25 17:29:41 -0700

    good luck

--- routes/splitwiseAuth.js
+++ routes/splitwiseAuth.js
@@ -1,21 +1,35 @@
 var express = require('express');
+var crypto = require('crypto');
+var OAuth = require('oauth-1.0a');
+var request = require('request');
 
 /* GET home page. */
 let splitwiseAuth = function(req, res) {
-    var AuthApi = require('splitwise-node');
-    var ConsumerKey = "o5zyoGbdvTni1rUw4OU4WbHQb94kPhJbui0NThYY";
-    var ConsumerSecret = "OgAl8B6IE7loMWO0oyqyO4HEOGQAvtLKXIO5THBR";
-    var userOAuthToken, userOAuthTokenSecret;
-    var authApi = new AuthApi(ConsumerKey, ConsumerSecret);
-    var userAuthUrl = authApi.getOAuthRequestToken()
-    .then(({ oAuthToken, oAuthTokenSecret }) => {
-        [userOAuthToken, userOAuthTokenSecret] = [oAuthToken, oAuthTokenSecret];
-        return api.getUserAuthorisationUrl(oAuthToken);
+    var oauth = OAuth({
+        consumer: {
+            key: 'o5zyoGbdvTni1rUw4OU4WbHQb94kPhJbui0NThYY',
+            secret: 'OgAl8B6IE7loMWO0oyqyO4HEOGQAvtLKXIO5THBR'
+        },
+        signature_method: 'HMAC-SHA1',
+        hash_function: function(base_string, key) {
+            return crypto.createHmac('sha1', key).update(base_string).digest('base64');
+        }
     });
+    var request_data = {
+        url: 'https://secure.splitwise.com/api/v3.0/get_request_token',
+        method: 'POST',
+        data: {
+            status: 'Hello Ladies + Gentlemen, a signed OAuth request!'
+        }
+    };
 
-    var splitwiseAPI = authApi.getSplitwiseApi(userOAuthToken, userOAuthTokenSecret);
-
-  res.render('index', { title: 'Express' });
+    request({
+        url: request_data.url,
+        method: request_data.method,
+    }, function(error, response, body) {
+        
+    res.render('index', { title: 'Express' });
+    });
 };
 
 module.exports = splitwiseAuth;
